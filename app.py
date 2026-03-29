78990673e9e8ab1a148d00497b67ce4e58fe9325
oestemar <oestemar_a24@yahoo.co.jp>
2026-03-29 12:39:14 +0900

    Add DB initialization code

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_connection():
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
         database=os.environ.get("DB_NAME"),
-        port=os.environ.get("DB_PORT")
+        port=int(os.environ.get("DB_PORT"))
     )
 
 #MySQLにテーブルがないときに自動作成するコード
