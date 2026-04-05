b32cdc6404b7989a26242f55f524ea33b9ad671c
oestemar <oestemar_a24@yahoo.co.jp>
2026-04-05 19:58:44 +0900

    Add DB initialization code

--- app.py
+++ app.py
@@ -195,25 +195,10 @@ def delete_confirm():
     conn.close()
 
 #Index画面
-@app.route("/register", methods=["GET"])
+@app.route("/index", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/display", methods=["GET"])
-def index():
-    return render_template("index.html")
-
-@app.route("/search", methods=["GET"])
-def index():
-    return render_template("index.html")
-
-@app.route("/upload", methods=["GET"])
-def index():
-    return render_template("index.html")
-
-@app.route("/delete", methods=["GET"])
-def index():
-    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
